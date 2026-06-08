7d005ddbf2d09feb505ca93ad79908eb2bb71293
a.muchkaev <cinside.a.muchkaev@crpt.ru>
2026-06-09 00:01:31 +0300

    init addEventListeners on Discord

--- src/main/java/com/bigmoji/config/JdaConfig.java
+++ src/main/java/com/bigmoji/config/JdaConfig.java
@@ -1,5 +1,6 @@
 package com.bigmoji.config;
 
+import com.bigmoji.discord.EmojiMessageListener;
 import jakarta.annotation.Nullable;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
@@ -12,12 +13,13 @@ import org.springframework.context.annotation.Configuration;
 public class JdaConfig {
   @Bean
   @Nullable
-  JDA jda(@Value("${bigmoji.discord.token:}") String token) throws InterruptedException {
+  JDA jda(@Value("${bigmoji.discord.token:}") String token, EmojiMessageListener listener) throws InterruptedException {
     if (token == null || token.isBlank()) {
       return null;
     }
     return JDABuilder.createDefault(token)
         .enableIntents(GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT)
+        .addEventListeners(listener)
         .build()
         .awaitReady();
   }
